c7464a366a056cbd24f62de91da61f8f6ce7c9bf
wuran <wuran@ucfgroup.com>
2017-11-13 20:01:08 +0800

    reinit ios module

---
+++ ios/ScannerModule/ScannerModule.h
@@ -0,0 +1,13 @@
+//
+//  ScannerModule.h
+//  ScannerModule
+//
+//  Created by sky on 2017/11/13.
+//  Copyright © 2017年 Sky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScannerModule : NSObject
+
+@end
---
+++ ios/ScannerModule/ScannerModule.m
@@ -0,0 +1,13 @@
+//
+//  ScannerModule.m
+//  ScannerModule
+//
+//  Created by sky on 2017/11/13.
+//  Copyright © 2017年 Sky. All rights reserved.
+//
+
+#import "ScannerModule.h"
+
+@implementation ScannerModule
+
+@end
